a1edbda31f4f0166794a143059095c800e9a269c
maniyedidi <yedidi-satya.manikanta@sixt.com>
2023-04-23 12:00:38 +0530

    added editor

--- src/pages/video-editor/index.js
+++ src/pages/video-editor/index.js
@@ -1,11 +1,4 @@
 import Header from '@/components/header';
-import {
-  Add,
-  CollectionsOutlined,
-  VideoCall,
-  VideoCameraFrontOutlined
-} from '@mui/icons-material';
-import { Chip } from '@mui/material';
 import VSideMenu from '@/components/v-side-menu';
 import VideoEditorTimeLine from '@/components/video-editor-timeline';
 
@@ -15,33 +8,7 @@ const VideoEditor = () => {
       <Header />
       <div className='grid grid-cols-[100px_1fr] '>
         <VSideMenu />
-        <div className='bg-[#FDFBFC] p-4 flex flex-col gap-3'>
-          <div className='flex flex-row gap-5'>
-            <Chip
-              variant='outlined'
-              className='px-2'
-              icon={<Add />}
-              label='Start from scratch'
-            />
-            <Chip
-              variant='outlined'
-              className='px-2'
-              icon={<CollectionsOutlined />}
-              label='Start from templates'
-            />
-            <Chip
-              variant='outlined'
-              className='px-2'
-              icon={<VideoCall />}
-              label='Start a recording'
-            />
-            <Chip
-              variant='outlined'
-              className='px-2'
-              icon={<VideoCameraFrontOutlined />}
-              label='Request a recording'
-            />
-          </div>
+        <div className='bg-[#FDFBFC] p-4 flex flex-col gap-3'>         
           <VideoEditorTimeLine />
         </div>
       </div>
